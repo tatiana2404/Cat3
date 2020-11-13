b892bd2a78ce9f2ca9e28ee306ade7ed060a447a
tatiana2404 <tane4ka2404@gmail.com>
2020-11-13 15:40:20 +0200

    Initial commit

---
+++ src/com/company/Cat.java
@@ -0,0 +1,75 @@
+package com.company;
+
+public class Cat {
+    private String name;
+    private int age;
+    private String color;
+    private int weight;
+    private String breed;
+
+    public Cat(){}
+
+    public Cat(String name, int age, String color, int weight, String breed){
+        this.name=name;
+        this.age=age;
+        this.color=color;
+        this.weight=weight;
+        this.breed=breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int getWeight(){
+        return  weight;
+    }
+    public void setWeight(int weight){
+        this.weight=weight;
+    }
+    public String getBreed(){
+        return breed;
+    }
+    public void setBreed(String breed){
+        this.breed=breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                ", weight=" + weight +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+    public static String oldest(Cat cat)
+    {
+        if(cat.age>5){
+            return cat.name+" old cat";
+        }
+        else {
+            return cat.name+ " young cat";
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat cat1=new Cat("Iriska", 5, "orange", 6,"Main coon");
+        Cat cat2=new Cat("Barsik", 1, "gray", 1, "British");
+        Cat cat3=new Cat("Sonya", 10, "black", 4, "Persian");
+
+        System.out.println(cat1.toString());
+        System.out.println(Cat.oldest(cat1));
+        System.out.println(cat2.toString());
+        System.out.println(Cat.oldest(cat2));
+        System.out.println(cat3.toString());
+        System.out.println(Cat.oldest(cat3));
+    }
+}
